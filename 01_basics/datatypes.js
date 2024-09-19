516669d3739cb68b1588970ee0d158ed3c59fad1
Dilkhush Yadav <142766846+this-is-dsk@users.noreply.github.com>
2024-09-19 15:35:37 +0000

    data types

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+//alert( 3 + 3 ) we r using node js not browser..
+
+let name = "dilkhush"
+let age = "20"
+let isLoggedIn = true
+
+
+// datatypes
+
+// number
+// boolean  true/false
+// String  ""
+// BigInt
+// null  standalone value
+// undefined
+// symbol
+
+
+// object
+
+
+console.log(typeof "kfhwfh")
+console.log(typeof 92)
+console.log(typeof null)
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144552
+let accountEmail = "dilkhush@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2  not allowed 
+
+accountEmail = "hello@gmail.com"
+accountPassword = "28277"
+accountCity = "bngluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
