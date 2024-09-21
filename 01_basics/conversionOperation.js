bb15ea5f9086891331ab2e144cc69168e8ddafc2
Dilkhush Yadav <142766846+this-is-dsk@users.noreply.github.com>
2024-09-21 14:50:34 +0000

    Conversion Of dataTypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,15 @@
+let score = "33"
+console.log(typeof score);
+
+let valueInnumber = Number(score)
+console.log(typeof valueInnumber)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+
+console.log(typeof stringNumber);
+
+
