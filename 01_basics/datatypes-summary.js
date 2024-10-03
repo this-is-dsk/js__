6f0c1218636facfe71784e04e761a2f6765607ad
Dilkhush Yadav <142766846+this-is-dsk@users.noreply.github.com>
2024-10-03 13:05:37 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,31 @@
+//  1. Primitive Data Types
+
+// 7 types---
+// 1.String
+// 2.Number
+// 3.Boolean
+// 4.NUll
+// 5.Undefined
+// 6.symobl
+// 7.BigInt
+
+
+
+
+// 2. non-primitive Data Types/reference type
+
+// Array,objects,Functions
+
+// javascript is a dynamic type language..
+
+
+// memory++++++++++++++++++++++++++++++++++++
+
+// stack(primitive), heap(non-primitive) 
+
+let myYoutubename = "this_is_dsk"
+
+let anothername = myYoutubename
+anothername = "cricmasala"
+console.log(anothername);
+console.log(myYoutubename);
