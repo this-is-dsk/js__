9f7e922e888f0bf1c30a83bebfb438d2fcdbb077
Dilkhush Yadav <142766846+this-is-dsk@users.noreply.github.com>
2024-10-03 13:10:25 +0000

    nums and maths

---
+++ 01_basics/nums_maths.js
@@ -0,0 +1,44 @@
+const score1 = 100
+
+const balance = new Number(400)
+console.log(balance);
+
+console.log(balance.toString())
+console.log(typeof balance)
+console.log(balance.toFixed(3)) 
+
+const anotherNumber = 109.456
+console.log(anotherNumber.toPrecision(3));
+
+
+const score2 = 109.456
+console.log(score.toPrecision(4));
+
+const anotherScore = 109.656
+console.log(anotherScore.toPrecision(3));
+
+// ++++++++++++maths+++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-34));
+console.log(Math.round(3.675));
+console.log(Math.ceil(3.675));
+console.log(Math.floor(3.675));
+console.log(Math.sqrt(9));
+console.log(Math.PI);
+
+console.log(Math.random()); // alwys output will be bw 0 and 1
+
+console.log((Math.floor(Math.random()*10)))
+
+min = 1
+max = 6
+console.log(Math.floor(Math.random() * (max - min +1) +min))
+
+
+
+
+
+
+
+
