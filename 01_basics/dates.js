4b00579c4b318315397f2b52ffe9d2926dcaaae5
Dilkhush Yadav <142766846+this-is-dsk@users.noreply.github.com>
2024-10-03 13:27:50 +0000

    dates in js

---
+++ 01_basics/dates.js
@@ -0,0 +1,17 @@
+let myDate = new Date()
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+
+
+let myCreatedDate = new Date(2003, 7, 4)
+console.log(myCreatedDate.toDateString());
+
+
+
+
+
