a571cf05d86576d4d2a9ce75a9add73055bd7854
Dilkhush Yadav <142766846+this-is-dsk@users.noreply.github.com>
2024-10-03 13:06:16 +0000

    string

---
+++ 01_basics/strings.js
@@ -0,0 +1,23 @@
+const name = "dilkhush"
+const repoCount = "20"
+
+// console.log(name+repoCount);
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const myGame = 'cricket'
+console.log(typeof myGame);
+console.log(typeof name);
+console.log(myGame[0]);
+console.log(myGame.__proto__);
+
+console.log(myGame.endsWith("dkk"));
+console.log(myGame.charAt(2));
+
+
+
+
+
+
+
+
