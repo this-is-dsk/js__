9e3e37f3d1705ef5a140b146f3981063c0104aa1
Dilkhush Yadav <142766846+this-is-dsk@users.noreply.github.com>
2024-10-04 14:48:43 +0000

    arrays

---
+++ 02_basics/arrays.js
@@ -0,0 +1,38 @@
+const myArr = [1, 2, 3, 4, 5]
+console.log(myArr);
+console.log(myArr[2]);
+
+
+
+                        //   ARRAY-METHODS
+
+// push - add a new value in array at last... myArr.push(6)
+// pop - remove the last element .... myArr.pop()
+// unshift - add an Element at the start ... myArr.unshift(9)
+// shift - remove the Element from the top or start myArr.shift()
+
+console.log(myArr.includes(90));
+console.log(myArr.indexOf(5));
+
+const myHeroes = ["Ghambir", "Kohli", "Rajat", "Deviliors", "Maxi", "Faf"]
+console.log("Original", myHeroes);
+
+
+const myNewarr = myHeroes.slice(2, 5)
+console.log(myNewarr);
+console.log("Slice", myHeroes);
+
+
+const myNewarr2 = myHeroes.splice(2, 5)
+console.log(myNewarr2);
+console.log("Splice", myHeroes);
+
+// most important -- differnce bw slice and aplice👋
+
+
+
+
+
+
+
+
