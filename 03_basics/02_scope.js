4f9964d648b91175d60ae902ab42705e47330d1d
Dilkhush Yadav <142766846+this-is-dsk@users.noreply.github.com>
2024-11-06 11:32:04 +0000

    scopes

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,16 @@
+let a = 20;
+var b = 11;
+// const c = 11;
+
+if (true) {
+    let a = 200;
+    var b = 29;
+    // const c = 11;
+    console.log("inner: "+ a);
+    
+}
+
+console.log(a);
+console.log(b);
+// console.log(c);
+
