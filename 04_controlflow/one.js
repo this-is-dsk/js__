f4d1dde89fbd338d26ec3c600f539f650c61cb1b
Dilkhush Yadav <142766846+this-is-dsk@users.noreply.github.com>
2024-11-07 12:52:53 +0000

    controlflow

---
+++ 04_controlflow/one.js
@@ -0,0 +1,15 @@
+const score = 78;
+
+if (score>33) {
+    const student = "pass";
+    console.log(`student reslut : ${student}`);   
+}
+
+const userLoggedIn = true
+const googleLogin = true
+const emailLgin = false
+
+if (userLoggedIn && googleLogin || emailLgin) {
+    console.log("you can buy course");
+    
+}
---
+++ 04_controlflow/switch.js
@@ -0,0 +1,51 @@
+const month = 3;
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;  
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("AUg");
+        break;
+    case 9:
+        console.log("Sept");
+        break;
+    case 10:
+        console.log("Oct");
+        break; 
+    case 11:
+        console.log("Nov");
+        break;
+    case 12:
+        console.log("Dec");
+        break;     
+    default:
+        console.log("enter valid number");
+        break;
+}
+
+// there are some truthy or falsy values which gives true or false in console -----
+
+// falsy values --
+// false, 0, -0,"", null , undefined, NaN
+
+// truthy values--
+// "0", 'false', " ", [], {}, function(){},
