d2b81bafc0c27ac867438af11d57172c8f53549a
Dilkhush Yadav <142766846+this-is-dsk@users.noreply.github.com>
2024-11-09 17:06:19 +0000

    loops

---
+++ 05_iterations/1.js
@@ -0,0 +1,24 @@
+// for
+
+for (let i= 0; i <= 10; i++) {
+    const element = i;   
+    console.log(i);
+}
+
+// while
+
+let num = 0;
+while (num<=10) {
+    console.log(`value of number is ${num}`);
+    num = num +2;
+}
+
+// do while
+
+let kohliCenturies = 1
+do {
+    console.log(`kohlis centuries is ${kohliCenturies}`);
+    kohliCenturies++
+    
+} while (kohliCenturies<81);
+
